d5f22971477cade634e6a476b6d15851a3fc6122
Nilson Ferreira Costa Júnior <nilson.developer@gmail.com>
2017-08-04 15:00:01 -0300

    Commit

--- src/NFC.SignalRPainelSenhas.MVC/Hubs/PainelSenhasHub.cs
+++ src/NFC.SignalRPainelSenhas.MVC/Hubs/PainelSenhasHub.cs
@@ -4,9 +4,9 @@ namespace NFC.SignalRPainelSenhas.MVC.Hubs
 {
     public class PainelSenhasHub : Hub
     {
-        public void InformarSenhaAtual(string guinche, string senha)
+        public void InformarSenhaAtual(string guiche, string senha)
         {
-            Clients.All.DispararSenhaAtual(guinche,senha);
+            Clients.All.DispararSenhaAtual(guiche,senha);
         }
     }
 }
